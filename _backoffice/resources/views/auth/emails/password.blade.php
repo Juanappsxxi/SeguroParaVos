e239c50fc4033668d4b2e71b942b629d1be6216f
Mike <miguel.sosa@globant.com>
2016-10-18 20:26:01 -0300

    Set forms and texts to spanish
    - Resize boxes to a better resolution

--- _backoffice/resources/views/auth/emails/password.blade.php
+++ _backoffice/resources/views/auth/emails/password.blade.php
@@ -1,1 +1,1 @@
-Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+Click para reestablecer la contraseña: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
