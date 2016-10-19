a4bfb032f84f7d99e9c3277e64fe57259b4df8c6
Mike <miguel.sosa@globant.com>
2016-10-19 20:47:35 -0300

    Renamed file for styles, added scripts.js

--- _backoffice/resources/views/layouts/master.blade.php
+++ _backoffice/resources/views/layouts/master.blade.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 	<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/estilo.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
 	@stack('styles')
 
@@ -43,6 +43,8 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<script src="{{ asset('js/scripts.js') }}"></script>
 
 	@stack('scripts')
 </body>
